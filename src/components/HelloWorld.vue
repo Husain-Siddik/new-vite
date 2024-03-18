6cc47a1c884749d50f8ba2fed13dcd0daa33d365
Husain-Siddik <ronysiddik21@gmail.com>
2024-03-19 01:54:08 +0600

    second

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue'
+defineProps({
+  msg:String,
+})
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+  <h1>{{ msg }}</h1>
+ 
+
+  <div class="card">
+    <button class="buton1" type="button" @click="count++">+ </button><br>
+    <button class="buton1" type="button" v-if="count > 0" @click="count--">-</button>
+    <h4>{{ count }}</h4>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+ 
+</div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+.buton1{
+  padding-bottom: 10px;
+  margin-top: 30px;
+}
+</style>
---
+++ src/components/arrey.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+    data: String
+});
+
+const header = ref('to do items')
+const items = ref([
+{id:1, level :'this is fun'},
+{id:2, level :'this is fun'},
+{id:3, level :'this is fun'},
+])
+
+
+
+</script>
+
+<template>
+  
+    <div>
+        <h3>{{ header }}</h3>
+    <ul className="arrey">
+        <li v-for="(item,id) in items" :key="id" >
+           ---{{ item.level }}-- {{item.id}} 
+        </li>
+    </ul>
+    <h2>{{ data }}</h2>
+
+    </div>
+
+</template>
+<style scoped>
+.arrey{
+    color: red;
+    list-style: none;
+   
+}
+</style>
